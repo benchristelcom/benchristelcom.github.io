dcd08400b66fe8dbcf1962d87b86780b4f771c0c
Ben Christel <benchristel@users.noreply.github.com>
2025-07-03 09:17:21 -0700

    Update arrows.js

--- src/assets/arrows.js
+++ src/assets/arrows.js
@@ -4,4 +4,6 @@ Understand more so you can think less.
 
 A universe that couldn't appear in consciousness wouldn't appear to exist at all.
 
+There are three types of people in the world: those who wear fun socks because they want to seem quirky and cool; those who don't wear fun socks because they want to seem serious and grown-up; and those who do wear fun socks because they received a pair for Christmas and don't care what other people think of them.
+
 `.trim().split("\n\n")
